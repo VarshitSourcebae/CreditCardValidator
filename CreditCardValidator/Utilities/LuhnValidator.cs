4117fc996bd51482f7822c20fbdea80337f129ab
meettrd <meet.a@tridhyatech.com>
2024-06-11 16:16:38 +0530

    Add LuhnValidator utility class for credit card validation

---
+++ CreditCardValidator/Utilities/LuhnValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace CreditCardValidator.Utilities
+{
+    public class LuhnValidator
+    {
+        public static bool IsValid(string creditCardNumber)
+        {
+            int sum = 0;
+            bool alternate = false;
+
+            for (int i = creditCardNumber.Length - 1; i >= 0; i--)
+            {
+                char[] nx = creditCardNumber.ToCharArray();
+                int n = int.Parse(nx[i].ToString());
+
+                if (alternate)
+                {
+                    n *= 2;
+                    if (n > 9)
+                    {
+                        n -= 9;
+                    }
+                }
+
+                sum += n;
+                alternate = !alternate;
+            }
+
+            return (sum % 10 == 0);
+        }
+    }
+}
